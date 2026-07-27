85c20ae1f1422d8783579505fa4a74caa3dd4bb1
Kea-Dev1 <kea349750@gmail.com>
2026-07-27 18:03:26 +0700

    frontend-configuration-static-link-edit

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::middleware('auth')->group(function () {
     });
     Route::get('/', function () {
         return view('dashboard.dashboard');
-    });
+    })->name('dashboard');
     Route::prefix('user')->group(function () {
         Route::get('/', function () {
             return view('users.index');
